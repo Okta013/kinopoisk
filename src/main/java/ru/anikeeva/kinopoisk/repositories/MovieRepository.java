6f785df7339c82e611cfd59826e70c287648ba47
okta013 <sindorayka@yandex.ru>
2024-07-31 14:57:19 +0300

    Add custom method to movieRepository and reviewRepository

--- src/main/java/ru/anikeeva/kinopoisk/repositories/MovieRepository.java
+++ src/main/java/ru/anikeeva/kinopoisk/repositories/MovieRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.anikeeva.kinopoisk.entities.Movie;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
+    List<Movie> findAllByGenre(String genre);
 }
--- src/main/java/ru/anikeeva/kinopoisk/repositories/ReviewRepository.java
+++ src/main/java/ru/anikeeva/kinopoisk/repositories/ReviewRepository.java
@@ -2,9 +2,12 @@ package ru.anikeeva.kinopoisk.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.anikeeva.kinopoisk.entities.Movie;
 import ru.anikeeva.kinopoisk.entities.Review;
+
+import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
-
+    List<Review> findAllByMovie(Movie movie);
 }
